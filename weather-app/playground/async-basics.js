f95c41ce92920615865ded840c7b6c9c825875f1
Teja Kantamneni <teja.kantamneni@gmail.com>
2018-06-04 22:20:41 -0400

    async basics

--- weather-app/playground/async-basics.js
+++ weather-app/playground/async-basics.js
@@ -5,4 +5,8 @@ setTimeout(() => {
 	console.log("setTimeout Callback Executed...");
 }, 2 * 1000);
 
+setTimeout(() => {
+	console.log("Timeout with zero delay...");
+}, 0)
+
 console.log("Closing App...");
